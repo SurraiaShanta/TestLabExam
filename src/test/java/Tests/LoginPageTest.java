448ebb1a175a0992db6b87af517100994b69f582
Surraia Akter Shanta <surraiaaktershanta@gmail.com>
2025-04-22 01:13:51 +0600

    Update LoginPageTest.java

--- src/test/java/Tests/LoginPageTest.java
+++ src/test/java/Tests/LoginPageTest.java
@@ -8,26 +8,30 @@ import org.testng.annotations.Test;
 
 public class LoginPageTest extends BaseTest {
 
-    @Test(priority = 2)
+     @Test(priority = 2)
     public void validlogin() throws InterruptedException {
+        driver.get("https://labsqajobs.qaharbor.com/login/");
         LoginPage log = new LoginPage();
-        log.login(" probaldighi53@gmail.com", "QAH123harbor");
+        log.login("Admin65432@gmail.com", "Admin65432");
         Thread.sleep(2000);
 
         String ActualURL=driver.getCurrentUrl();
+        Thread.sleep(2000);
         String ExpectedURL = "https://labsqajobs.qaharbor.com/account/";
         Assert.assertEquals(ActualURL,ExpectedURL,"Login error");
     }
 
     @Test(priority = 1)
     public void invalidLogin() throws InterruptedException {
+        driver.get("https://labsqajobs.qaharbor.com/login/");
             LoginPage log = new LoginPage();
-            log.login(" probaldighi53@gmail.com", "QAH123harborrr");
+            log.login(" QAharbor53@gmail.com", "QAH123harborrr");
             Thread.sleep(2000);
 
             String ActualURL=driver.getCurrentUrl();
+            Thread.sleep(2000);
             String ExpectedURL = "https://labsqajobs.qaharbor.com/account/";
-            Assert.assertEquals(ActualURL,ExpectedURL,"Login error");
+            Assert.assertNotEquals(ActualURL,ExpectedURL,"Login error");
     }
 
 
